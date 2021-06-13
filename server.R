fd9aec55ec162dfc780eba8967b5ae833b9d242e
Thinzar <thinzar.oo@n2nconnect.com>
2021-06-13 18:18:16 +0800

    updated codes

--- server.R
+++ server.R
@@ -8,19 +8,54 @@
 #
 
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
+# Define server logic
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    # ----------------------------------
+    #general functions
+    stock_list_df <- read.csv('data/FBM_KLCI_stocks_list.csv')
+    all_stocks_df <- read.csv('data/FBM_KLCI_historical_price_all_stocks.csv')
+    
+    #calculate price trend
+    plotLineChart = function (maindf, mSymbol) {
+      p1 <- ggplot(filter(maindf, Symbol == mSymbol), mapping = aes(Date, Close, group = 1)) +
+        geom_line(color="#69b3a2") +
+        labs(x = "Date Range", y = "Close Price", 
+             title = mSymbol)  
+      p1 + theme(axis.text.x=element_text(angle = 90, hjust = 0))
+      print(p1)
+    }
+    
+    get_stocks_by_sector <- function(sector_id){
+        stocks_by_sector <- stock_list_df %>% filter(SectorId == sector_id) %>% select(Symbol)
+    }
+    # ----------------------------------
+    
+    
+    # ----------------------------------
+    #panel 5
+    sector_list <- c("Banking" = "1",
+      "Food & Beverages" = "2",
+      "Healthcare Equipment & Services" = "3",
+      "Plantation" = "4",
+      "Telecommunications Service Providers" = "5")
+    
+    interval_list <- c("All time" = "1",
+                       "First Wave" = "2",
+                       "Second Wave" = "3",
+                       "Third Wave" = "4")
+    
+    output$user_selection_analysis <- renderUI({
+        sector_str <- paste("Sector: ", names(sector_list)[sector_list == input$sector_id])
+        interval_str <- paste("Interval: ",  names(interval_list)[interval_list == input$interval_id])
+        stock_list <- get_stocks_by_sector(input$sector_id)
+        stock_list_str <- paste("List of stocks: ", toString(stock_list))
+        
+        HTML(paste(sector_str, interval_str, stock_list_str, sep = '<br/>'))
     })
-
+    
+    
+    # ----------------------------------
 })
--- ui.R
+++ ui.R
@@ -24,11 +24,32 @@ shinyUI(navbarPage(title = "Starry Nine",
                    
                    # ----------------------------------
                    # tab panel 4 - Stocks Comparison
-                   tabPanel("Stocks Comparison",
+                   tabPanel("Stocks Comparison"
                    ),
                    
                    # tab panel 5 - Analysis by sector
-                   tabPanel("Analysis by sector"
+                   tabPanel("Analysis",
+                            titlePanel("Market Analysis by Sector"),
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput("sector_id", "Sector",
+                                            choices = c("Banking" = "1",
+                                                        "Food & Beverages" = "2",
+                                                        "Healthcare Equipment & Services" = "3",
+                                                        "Plantation" = "4",
+                                                        "Telecommunications Service Providers" = "5")),
+                                radioButtons("interval_id", "Select interval",
+                                             choices = c("All time" = "1",
+                                                         "First Wave" = "2",
+                                                         "Second Wave" = "3",
+                                                         "Third Wave" = "4"))
+                              ),
+                              mainPanel(
+                                helpText("Select the sector you wished to explore."),
+                                htmlOutput("user_selection_analysis"),
+                                #plotOutput("PlotAnalysisBySector")
+                              )
+                            )
                    ),
                    # ----------------------------------
                    
@@ -37,5 +58,4 @@ shinyUI(navbarPage(title = "Starry Nine",
                             includeHTML("about.html")
                    )
                    # ----------------------------------
-                   
 ))
